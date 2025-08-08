1f57485ac73700dbab5bd118a8ff3e59780bd372
siraj <74810515+cmdsiraj@users.noreply.github.com>
2025-08-07 22:23:49 -0400

    Update configLoader.js

--- utils/configLoader.js
+++ utils/configLoader.js
@@ -1,6 +1,7 @@
 let CONFIG = {};
 
-export async function loadConfig(path = "../config.json") {
+const basePath = window.location.pathname.replace(/\/[^/]*$/, '/');
+export async function loadConfig(path = `${basePath}config.json`) {
   const response = await fetch(path);
   if (!response.ok) throw new Error("Failed to load config");
   CONFIG = await response.json();
